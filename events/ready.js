ad1c6bc284b602d4157d1e27ef0af0613c28b12a
Ultra Cookie <59873833+UltraCookie1780@users.noreply.github.com>
2021-02-15 20:57:49 +0100

    Changed bot activity

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,4 @@
-//here the event starts
 module.exports = client => {
-    console.log(`Discord Bot ${client.user.tag} is online!`); //log when ready aka the bot usable
-    client.user.setActivity(`over the cookies.`, { type: "WATCHING"}) //first parameter, is the status, second is an object with type which can be: "PLAYING", "WATCHING", "LISTENING", "STREAMING" (where you need to add a , and then url: "https://twitch.tv/#")
+    console.log(`Discord Bot ${client.user.tag} is online!`);
+    client.user.setActivity(`over the cookies.`, { type: "WATCHING"}) //"PLAYING", "WATCHING", "LISTENING", "STREAMING"
 }
