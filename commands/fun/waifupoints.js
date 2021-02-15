0ce653ee8e8884ec253625b57dfce92e7535dda1
Ultra Cookie <59873833+UltraCookie1780@users.noreply.github.com>
2021-02-15 20:19:55 +0100

    Added waifupoints
    Waifupoints are required to collect waifus. Can be configured in config.json

---
+++ commands/fun/waifupoints.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: "waifupoints",
+    category: "fun",
+    aliases: ["wp"],
+    cooldown: 2,
+    usage: "waifupoints",
+    description: "Shows your waifupoints",
+    run: async (client, message, args, user, text, prefix, waifuPoints) => {	
+        var target = message.mentions.users.first() || message.author;
+        var wp = waifuPoints.get(target.id);
+        if (!wp) {
+            const emoji1 = client.emojis.cache.find(emoji => emoji.name === "popukosad");
+            message.reply(`this user has no waifupoints.${emoji1}`);
+        } else {
+            const emoji2 = client.emojis.cache.find(emoji => emoji.name === "zerotwohappy");
+            message.channel.send(`${target.username} has ${wp.messages}${emoji2} waifupoints.`);
+        }
+    }
+}
