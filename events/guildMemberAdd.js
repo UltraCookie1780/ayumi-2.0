52209f52edddf57a518e5742ecdcca98b8e7765c
Ultra Cookie <59873833+UltraCookie1780@users.noreply.github.com>
2021-02-17 20:44:23 +0100

    Removed comments

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,3 @@
-//here the event starts
 module.exports = async (client, member) => {
     console.log(member + "joined in: " + member.guild.name)
 };
--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -1,4 +1,3 @@
-//here the event start
 module.exports = async (client, reaction, user) => {
     if (reaction.message.partial) await reaction.message.fetch();
     if (reaction.partial) await reaction.fetch();
