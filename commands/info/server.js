682045a11ea2bc2b929c42131901cb16f7b3b9ea
Ultra Cookie <59873833+UltraCookie1780@users.noreply.github.com>
2021-02-17 18:45:36 +0100

    Added server info command

---
+++ commands/info/server.js
@@ -0,0 +1,29 @@
+module.exports = {
+    name: "server",
+    category: "info",
+    aliases: ["s", "serverinfo"],
+    cooldown: 5,
+    usage: "server",
+    description: "Displays all information about the server",
+    run: async (client, message, args, user, text, prefix, waifuPoints, servermessages) => {	
+        const { MessageEmbed } = require("discord.js");
+        const { Servers } = require("../../rsc/connect");
+        const verified = await Servers.findOne({ where: { server_id: message.guild.id } });
+        if (!verified) {
+            return message.reply(`an error occurred, please try again.`);
+        }
+        const embed = new MessageEmbed()
+        .setColor("BLACK")
+        .setThumbnail(client.user.displayAvatarURL())
+        .setTitle("Server: " + message.guild.name)
+        .addField("Name:", "`" + message.guild.name +"`")
+        .addFields(
+            { name: "Members:", value: "`" + message.guild.memberCount + "`", inline: true},
+            { name: "Messages:", value: "`" + servermessages.getb(message.guild.id) + "`", inline: true},
+            { name: "Verified", value: verified.verified, inline: true },
+
+        )
+        .setFooter("Owner: " + message.guild.owner.displayName);
+        message.channel.send(embed)
+    }
+}
