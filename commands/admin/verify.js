362a5821ae6fe902809f2e12451fee10c98ec2ef
Ultra Cookie <59873833+UltraCookie1780@users.noreply.github.com>
2021-02-17 18:43:47 +0100

    Added server verification

---
+++ commands/admin/verify.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: "verify",
+    category: "admin",
+    aliases: ["v", "verifyserver"],
+    cooldown: 0,
+    owner: true,
+    usage: "verify <server-id>",
+    description: "Gives a server the verified status",
+    run: async (client, message, args, user, text, prefix, waifuPoints, servermessages) => {
+        if (args == "") {
+            message.reply(`please provide a valid guild id.`);
+        } else if (servermessages.get(message.guild.id)) {
+            const { Users, Waifus, UserWaifus, Servers } = require('../../rsc/connect');
+            const server = await Servers.findOne({ where: { server_id: message.guild.id } });
+            await server.verify(message.guild.id);
+            message.reply(`the guild has been successfully verified!`);
+        }
+    }
+}
