9162266ae6ce5262f2d56410d3e551a2165eb2d3
Ultra Cookie <59873833+UltraCookie1780@users.noreply.github.com>
2021-02-18 13:45:10 +0100

    Removed comments

--- handlers/events.js
+++ handlers/events.js
@@ -1,30 +1,28 @@
-const fs = require("fs"); //requireing, the module for reading files 
-const ascii = require("ascii-table"); //requiring ascii-table which is a great tool for creating ascii tables
+const fs = require("fs");
+const ascii = require("ascii-table");
 
-let table = new ascii("Events"); //creating a new table with the name "Commands"
+let table = new ascii("Events");
 table.setHeading("Events", "Load status");
 
-module.exports = async (client) => {
-    let theevents; //global variable
-    fs.readdirSync("./events/").forEach(file => { //reading each command
-        theevents =  fs.readdirSync(`./events/`).filter(file => file.endsWith(".js")); //it will be only a command if it ends with .js
-        fs.readdir("./events/", (err, files) => { //for each file we will "LOAD THE EVENT"
-            if (err) return console.error(err); //if an error log it
-                    const event = require(`../events/${file}`); //create the event from the filename
-                    let eventName = file.split(".")[0]; //get the eventname from it
-                    theevents = eventName; //set it into the global variable
-                    client.on(eventName, event.bind(null, client)); //LOAD THE EVENT
+module.exports = async (client, client2) => {
+    let theevents;
+    fs.readdirSync("./events/").forEach(file => {
+        theevents =  fs.readdirSync(`./events/`).filter(file => file.endsWith(".js"));
+        fs.readdir("./events/", (err, files) => {
+            if (err) return console.error(err);
+                    const event = require(`../events/${file}`);
+                    let eventName = file.split(".")[0];
+                    theevents = eventName;
+                    client.on(eventName, event.bind(null, client));
             });
     });
-    
-    //now we have an array for all events in the event folder, we can load it in loop and pass it onto our beautiful table
     for(let i = 0; i< theevents.length; i++){
         try {
-            table.addRow(theevents[i], 'Ready'); //log in table ready
+            table.addRow(theevents[i], 'Ready');
         } catch (error) {
-            console.error(error.stack); // If there is an error, console log the error stack message
+            console.error(error.stack);
         }
     }
-    console.log(table.toString()); //showing the table
-	console.log("Logging into the BOT..."); //showing loading status
+    console.log(table.toString());
+	console.log("Logging into the BOT...");
 }
