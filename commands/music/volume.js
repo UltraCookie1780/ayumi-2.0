c0d3b9a30aa8d53d6c5fff793ca592a1debd8e3a
Ultra Cookie <59873833+UltraCookie1780@users.noreply.github.com>
2021-03-06 19:24:19 +0100

    Bugfix

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -12,7 +12,7 @@ module.exports = {
         const q = client.queue.get(message.guild.id);
         if (!q) return message.reply(`the queue is empty.`);
         if (!q.connection) return message.reply(`the queue is empty.`);
-        if (!args[0]) return message.channel.send(`🔊  **|**  Volume is: **\`${q.volume}%\``);
+        if (!args[0]) return message.channel.send(`🔊  **|**  Volume is: \`${q.volume}%\``);
         if (isNaN(args[0])) return message.channel.send(`:notes: Please provide a valid number.`).catch(err => console.log(err));
         if (parseInt(args[0]) > 150 || (args[0]) < 0) return message.reply(`you need to provide a number between 150 and 0.`);
         q.volume = args[0];
